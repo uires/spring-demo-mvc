853899c59e4c597ee93f851908460657ad397c75
uires <uiresdeivide1@gmail.com>
2018-08-11 00:17:02 -0300

    mapping request to get params in url

--- src/br/com/mvcdemo/controller/HelloWorldController.java
+++ src/br/com/mvcdemo/controller/HelloWorldController.java
@@ -2,14 +2,20 @@ package br.com.mvcdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloWorldController {
-	
+
 	// preciso de um método no controlador para devolver o form incial
 	@RequestMapping("/showform")
 	public String showForm() {
 		return "helloworld-form";
 	}
-	// preciso de um método processar a requisição (action) 
+
+	// preciso de um método processar a requisição (action)
+	@RequestMapping(value = "/processForm", method = RequestMethod.GET)
+	public String processForm() {
+		return "helloworld";
+	}
 }
