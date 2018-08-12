5a3e275894eceb4e3a72fba69c9fd3c34faa2879
uires <uiresdeivide1@gmail.com>
2018-08-11 23:40:58 -0300

    new controller class

---
+++ src/br/com/mvcdemo/controller/EstudanteController.java
@@ -0,0 +1,21 @@
+package br.com.mvcdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.mvcdemo.entity.Estudante;
+
+@Controller
+@RequestMapping("/estudante")
+public class EstudanteController {
+	@RequestMapping("/showForm")
+	public String showForm(Model model) {
+		// instance object estudante
+		Estudante estudante = new Estudante();
+
+		//	add student object to the model object	
+		model.addAttribute("estudante", estudante);
+		return "formulario-estudante";
+	}
+}
