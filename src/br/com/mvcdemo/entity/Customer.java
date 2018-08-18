ed68bd20208d3183d9c00ffb256459183ae31f5b
uires <uiresdeivide1@gmail.com>
2018-08-17 22:35:47 -0300

    adicionando atributos, validando com bean validator e métodos getters e setters

--- src/br/com/mvcdemo/entity/Customer.java
+++ src/br/com/mvcdemo/entity/Customer.java
@@ -1,5 +1,39 @@
 package br.com.mvcdemo.entity;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Customer {
+
+	private String firstName;
+	@NotNull(message = "Esse campo é obrigatório")
+	@Size(min = 3, max = 30, message = "Esse campo tem que ter no mínimo " 
+			+ "3 caracteres e no máximo 30 caracteres")
+	private String lastName;
+	
+	public Customer() {}
+	
+	public Customer(String firstName,
+			@NotNull(message = "Esse campo é obrigatório") @Size(min = 3, max = 30, message = "Esse campo tem que ter no mínimo 3 caracteres e no máximo 30 caracteres") String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public void setFirstName(String name) {
+		this.firstName = name;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 }
