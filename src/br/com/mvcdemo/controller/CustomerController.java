2911ac2d984159422e01c7f963fb9dd3ae489132
uires <uiresdeivide1@gmail.com>
2018-08-18 18:10:58 -0300

    pre-processing data binder, para validar espaço em branco em String

--- src/br/com/mvcdemo/controller/CustomerController.java
+++ src/br/com/mvcdemo/controller/CustomerController.java
@@ -2,9 +2,11 @@ package br.com.mvcdemo.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,17 +17,23 @@ import br.com.mvcdemo.entity.Customer;
 @RequestMapping("/customer")
 public class CustomerController {
 
+	@InitBinder
+	public void initBinder(WebDataBinder theDataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		theDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
 	@RequestMapping("/showForm")
 	public ModelAndView showForm() {
 		return new ModelAndView("customer/customer-form").addObject("customer", new Customer());
 	}
-	
+
 	@RequestMapping(value = "/processForm", method = RequestMethod.POST)
-	public ModelAndView processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult result) {
-		if(result.hasErrors()) {
+	public ModelAndView processForm(@Valid Customer theCustomer, BindingResult result) {
+		if (result.hasErrors()) {
 			return new ModelAndView("customer/customer-form");
 		}
-		return new ModelAndView();
+		return new ModelAndView("customer/customer-confirmacao").addObject("customer", theCustomer);
 	}
-	
+
 }
