d5474e5650a1378d4051f57420491c68ac76961e
uires <uiresdeivide1@gmail.com>
2018-08-19 20:51:28 -0300

    lógica de validação

--- src/br/com/mvcdemo/controller/validation/CourseCodeValidationClass.java
+++ src/br/com/mvcdemo/controller/validation/CourseCodeValidationClass.java
@@ -1,5 +1,24 @@
 package br.com.mvcdemo.controller.validation;
 
-public class CourseCodeValidationClass {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import lombok.val;
+
+public class CourseCodeValidationClass implements ConstraintValidator<CourseCode, String> {
+
+	private String coursePrefix;
+
+	@Override
+	public void initialize(CourseCode constraintAnnotation) {
+		coursePrefix = constraintAnnotation.value();
+	}
+
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		if (value.startsWith(coursePrefix) == true) {
+			return true;
+		}
+		return false;
+	}
 
 }
