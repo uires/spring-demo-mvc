1b6cb9bb9b4e83fc42981d4bf3cde6cff33eb099
uires <uiresdeivide1@gmail.com>
2018-08-19 20:51:14 -0300

    alteração na anotação

--- src/br/com/mvcdemo/controller/validation/CourseCode.java
+++ src/br/com/mvcdemo/controller/validation/CourseCode.java
@@ -11,7 +11,7 @@ import javax.validation.Payload;
 @Constraint(validatedBy = CourseCodeValidationClass.class)
 @Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface CurseCode {
+public @interface CourseCode {
 	
 	public String value() default "LUV";
 	
